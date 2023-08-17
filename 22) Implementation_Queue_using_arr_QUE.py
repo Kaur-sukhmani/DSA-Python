e6722a1e99371446e792b1f3af8167ffa5b55453
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2023-08-17 21:57:41 +0530

    Linked list

---
+++ 22) Implementation_Queue_using_arr_QUE.py
@@ -0,0 +1,50 @@
+# User function Template for python3
+
+class MyQueue:
+
+    def __init__(self):
+        self.arr = []
+        self.size = 0
+        self.head = 0
+
+    # Function to push an element x in a queue.
+    def push(self, x):
+        self.arr.append(x)
+        self.size += 1
+
+    # Function to pop an element from queue and return that element.
+    def pop(self):
+        if self.size == 0:
+            return -1
+        else:
+            top = self.arr[self.head]
+            self.head += 1
+            self.size -= 1
+
+            return top
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = MyQueue()
+        q = int(input())
+        q1 = list(map(int, input().split()))
+        i = 0
+        while (i < len(q1)):
+            if (q1[i] == 1):
+                s.push(q1[i + 1])
+                i = i + 2
+            elif (q1[i] == 2):
+                print(s.pop(), end=" ")
+                i = i + 1
+            elif (s.isEmpty()):
+                print(-1)
+                i = i + 1
+        print()
+
+        # } Driver Code Ends
